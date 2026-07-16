5742a7b56fedc2d1a9edafa82a47f12ef492d308
Lorena <lorbeafus@gmail.com>
2026-07-16 17:06:50 -0300

    fix

--- lib/features/settings/presentation/screens/sponsors_management_screen.dart
+++ lib/features/settings/presentation/screens/sponsors_management_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 import '../../../../core/theme/app_spacing.dart';
 import '../../../../core/theme/app_theme_colors.dart';
